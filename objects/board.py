02c1a9c0642c593d2d23689a217817bc7b9ad582
bp82125 <103567877+bp82125@users.noreply.github.com>
2023-10-04 21:38:53 +0700

    readded the base codes

---
+++ objects/board.py
@@ -0,0 +1,21 @@
+from constants import GRID_SIZE
+
+
+class Board:
+    def __init__(self, rows=None, cols=None, areas=None):
+        if rows is None or cols is None or areas is None:
+            self.rows = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
+            self.cols = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
+            self.areas = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
+        else:
+            self.rows = rows
+            self.rows = cols
+            self.areas = areas
+
+    def print(self):
+        print("Rows: ")
+        print(self.rows)
+        print("Cols: ")
+        print(self.cols)
+        print("Areas: ")
+        print(self.areas)
