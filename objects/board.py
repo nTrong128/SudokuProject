f753fd12b95834995d5522e9c9a85fa89be8f709
Michael-Ngn <nguyenphucky1004@gmail.com>
2023-10-05 18:06:10 +0700

    Update board.py

--- objects/board.py
+++ objects/board.py
@@ -9,15 +9,15 @@ from utils.calculate_stuff import map_to_area_index
 class Board:
     def __init__(self, rows=None, cols=None, areas=None):
         if rows is None or cols is None or areas is None:
-            self.rows = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
+            self.rows = {i: [1 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
             self.cols = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
             self.areas = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
         else:
             self.rows = rows
             self.rows = cols
             self.areas = areas
-
-        self.fixed_values: Dict[Coord, int] = {}
+        self.fitness_evaluation=0
+        self.fixed_values: Dict[int, int] = {}
 
     def print(self):
         print("Rows: ")
@@ -33,9 +33,14 @@ class Board:
         for coord in self.fixed_values:
             print(f'{coord}: {self.fixed_values[coord]}')
 
-    def add_value_by_coord(self, coord: Coord, value: int):
-        self.rows[coord.row][coord.col] = value
-        self.cols[coord.col][coord.row] = value
-        area_index, value_index = map_to_area_index(coord)
-        self.areas[area_index][value_index] = value
-        self.fixed_values[coord] = value
+    # def add_value_by_coord(self, coord: Coord, value: int):
+    #     self.rows[coord.row][coord.col] = value
+    #     self.cols[coord.col][coord.row] = value
+    #     area_index, value_index = map_to_area_index(coord)
+    #     self.areas[area_index][value_index] = value
+    #     self.fixed_values[coord] = value
+    def fitness(self):
+        count_duplicate=lambda arr:len([x for x in arr if x!=0]) - len(set(x for x in arr if x!=0))
+        for index in range(GRID_SIZE):
+            self.fitness_evaluation+=count_duplicate(self.rows[index]) +count_duplicate(self.cols[index])
+        return self.fitness_evaluation
