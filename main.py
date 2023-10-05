b8d337e4a65140497674eea6292e6c1483dcc445
bp82125 <103567877+bp82125@users.noreply.github.com>
2023-10-05 23:05:01 +0700

    - adjust the call in main to make it fit with the new read_from_file()

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from utils.read_file import read_from_file
 
 if __name__ == "__main__":
-    board = read_from_file("sudoku_file/input1.txt")
+    board = read_from_file("input1.txt")
     board.print()
