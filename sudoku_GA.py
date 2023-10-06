d2894067319762cf611185526cf533eb0ab2127c
Vitris128 <lenhattrong128@gmail.com>
2023-10-06 10:57:23 +0700

    Modify number to constant value.

--- sudoku_GA.py
+++ sudoku_GA.py
@@ -1,5 +1,6 @@
 import random
 
+from constants import GRID_SIZE
 from objects.board import Board
 
 
@@ -8,16 +9,16 @@ def fillArea(sudoku_Board: Board) -> None:
     """
     Fills the areas of the sudoku board with random values
     """
-    for area in range(0, 9):
-        number = [x for x in range(1, 10) if x not in sudoku_Board.areas[area]]
-        for cell in range(0, 9):
+    for area in range(GRID_SIZE):
+        number = [x for x in range(1, GRID_SIZE+1) if x not in sudoku_Board.areas[area]]
+        for cell in range(GRID_SIZE):
             if sudoku_Board.areas[area][cell] == 0:
                 value = random.choice(number)
                 sudoku_Board.areas[area][cell] = value
                 number.remove(value)
 
-        top_left_index_Col = area * 3 // 9 * 3
-        top_left_index_Row = area * 3 % 9
+        top_left_index_Col = area * 3 // GRID_SIZE * 3
+        top_left_index_Row = area * 3 % GRID_SIZE
 
         """
         filling Rows in Board with values from the Areas
