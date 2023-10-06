16ea4a1cf1739ee12597bd33ef7d2839fa193b45
Michael-Ngn <nguyenphucky1004@gmail.com>
2023-10-06 23:45:14 +0700

    doing

--- objects/board.py
+++ objects/board.py
@@ -1,15 +1,14 @@
 from collections import namedtuple
 from typing import Dict
-
+import random
 from constants import GRID_SIZE
 from objects.coordinate import Coord
 from utils.calculate_stuff import map_to_area_index
 
-
 class Board:
     def __init__(self, rows=None, cols=None, areas=None):
         if rows is None or cols is None or areas is None:
-            self.rows = {i: [1 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
+            self.rows = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
             self.cols = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
             self.areas = {i: [0 for i in range(GRID_SIZE)] for i in range(GRID_SIZE)}
         else:
@@ -22,6 +21,7 @@ class Board:
     
 
     def print(self):
+        print(self.fitness_evaluation)
         print("Rows: ")
         for row in self.rows:
             print(f'{row}: {self.rows[row]}')
@@ -46,3 +46,35 @@ class Board:
         for index in range(GRID_SIZE):
             self.fitness_evaluation+=count_duplicate(self.rows[index]) +count_duplicate(self.cols[index])
         return self.fitness_evaluation
+
+    def fillArea(self):
+        """
+        Fills the areas of the sudoku board with random values
+        """
+        for area in range(0, GRID_SIZE):
+            number = [x for x in range(1, GRID_SIZE + 1) if x not in self.areas[area]]
+            for cell in range(0, GRID_SIZE):
+                if self.areas[area][cell] == 0:
+                    value = random.choice(number)
+                    self.areas[area][cell] = value
+                    number.remove(value)
+            top_left_index_Col = area * 3 // GRID_SIZE * 3
+            top_left_index_Row = area * 3 % GRID_SIZE
+
+            """
+            filling Rows in Board with values from the Areas
+            """
+            counter = 0
+            for i in range(top_left_index_Col, top_left_index_Col + 3):
+                for j in range(top_left_index_Row, top_left_index_Row + 3):
+                    self.rows[i][j] = self.areas[area][counter]
+                    counter += 1
+            """
+            filling Cols in Board with values from the Areas
+            """
+            counter = 0
+            for i in range(top_left_index_Row, top_left_index_Row + 3):
+                for j in range(top_left_index_Col, top_left_index_Col + 3):
+                    self.cols[i][j] = self.areas[area][counter]
+                    counter += 1
+        return self
--- sudoku_GA.py
+++ sudoku_GA.py
@@ -1,28 +1,19 @@
-import random
 
+from SudokuProject.constants import GRID_SIZE
 from objects.board import Board
 
+def createPopulation(size: int):
+    population = {Board().fillArea() for _ in range(size)}
+    return population
 
-def fillArea(sudoku_Board: Board):
-    for area in range(0, 9):
-        for cell in range(0, 9):
-            i = random.randint(1, 9)
-            while sudoku_Board.areas[area][cell] == 0:
-                value = random.randint(1, 9)
-                if value not in sudoku_Board.areas[area][::]:
-                    sudoku_Board.areas[area][cell] = value
+def ranking(population):
+    return sorted(population, key=lambda individual: individual.fitness())
+gernation=createPopulation(5)
+print(gernation)
+for i in gernation:
+    print(i.print())
 
-        top_left_index_Col = area * 3 // 9 * 3
-        top_left_index_Row = area * 3 % 9
+print('-------------------------')
+for i in ranking(gernation):
+    print(i.print())
 
-        counter = 0
-        for i in range(top_left_index_Col, top_left_index_Col + 3):
-            for j in range(top_left_index_Row, top_left_index_Row + 3):
-                sudoku_Board.rows[i][j] = sudoku_Board.areas[area][counter]
-                counter += 1
-
-        counter = 0
-        for i in range(top_left_index_Row, top_left_index_Row + 3):
-            for j in range(top_left_index_Col, top_left_index_Col + 3):
-                sudoku_Board.cols[i][j] = sudoku_Board.areas[area][counter]
-                counter += 1
