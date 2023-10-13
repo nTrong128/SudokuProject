280e5cbfed2145434a8550eab3920f771e4e721b
Vitris128 <lenhattrong128@gmail.com>
2023-10-13 14:39:57 +0700

    Rebuild main func.

--- main.py
+++ main.py
@@ -1,39 +1,16 @@
 import time
-
-from objects.board import Board
-from sudoku_GA import create_population, create_children, new_create_children
-from utils.graph import draw_graph_scores
+from sudoku_GA import sudoku_GA
 from utils.read_file import read_from_file
+from constants import POPULATION_SIZE,CHILDREN_SIZE,SELECTION_RATE,MAX_GENERATION
 
-start_time = time.time()
-if __name__ == "__main__":
-    board = read_from_file("easy.txt")
-    population_size = 1000
-    children_size = 4
-    population: list[Board] = create_population(board, population_size)
-    max_generation = 250
-    selection_rate = 0.25
-    random_selection_rate = 0.25
-    min_evaluation = Board()
-    max_evaluation_graph = []
-    min_evaluation_graph = []
+def main():
+    start_time = time.time()
+    sudoku_board = read_from_file("default.txt")
 
-    for i in range(max_generation):
-        population = new_create_children(population, children_size, selection_rate, random_selection_rate)
-        print("Generation :", i + 1)
-        print("Number of individuals: ", len(population))
-        min_evaluation = min(population, key=lambda x: x.fitness_evaluation)
-        max_evaluation = max(population, key=lambda x: x.fitness_evaluation)
-        print("Max evaluation: ", max_evaluation.fitness_evaluation)
-        max_evaluation_graph.append(max_evaluation.fitness_evaluation)
-        print("Min evaluation: ", min_evaluation.fitness_evaluation)
-        min_evaluation_graph.append(min_evaluation.fitness_evaluation)
-        if min_evaluation.fitness_evaluation == 0:
-            print("Solved")
-            min_evaluation.print_matrix()
-            break
-    draw_graph_scores(min_evaluation_graph, max_evaluation_graph)
+    sudoku_GA(sudoku_board, POPULATION_SIZE, CHILDREN_SIZE, SELECTION_RATE, MAX_GENERATION, True)
 
     end_time = time.time()
-    print("The time of execution of above program is :",
-          (end_time - start_time) * 10 ** 3, "ms")
+    print("The time of execution of above program is :",(end_time - start_time) * 10 ** 3, "ms")
+
+if __name__ == "__main__":
+    main()
